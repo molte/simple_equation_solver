aba196f71e8616487e4b85b88e34ec3a3345b26f
Molte Emil Strange Andersen <molte93@gmail.com>
2010-03-29 15:10:06 +0200

    add tests

---
+++ test_helpers.rb
@@ -0,0 +1,30 @@
+module TestHelpers
+  def equation_system(name, &block)
+    tester = EquationSystemTest.new(name)
+    tester.instance_eval(&block)
+    tester.test(self)
+  end
+  
+  class EquationSystemTest
+    def initialize(name)
+      @equations = []
+      @name = name
+    end
+    
+    def equation(eq)
+      @equations << eq
+    end
+    
+    def solution(variables)
+      @solution = variables
+    end
+    
+    def test(test_class)
+      test_class.class_eval <<-EOT
+        def test_#{@name}_equation_system
+          assert_equal(#{@solution.inspect}, EquationSystem.new(*#{@equations.inspect}).solution, "Equation system could not be solved.")
+        end
+      EOT
+    end
+  end
+end
